6c0cb4f379cfb9f1b3a0e59538742171826b045e
carlpinto25 <carlpinto25@gmail.com>
2026-03-27 21:37:09 +0530

    redirct fic

--- apps/web/src/components/AuthScreen.tsx
+++ apps/web/src/components/AuthScreen.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/react";
 export default function AuthScreen() {
     return (
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100dvh" }}>
-            <SignIn />
+            <SignIn fallbackRedirectUrl={typeof window !== 'undefined' ? window.location.href : '/'} signUpFallbackRedirectUrl={typeof window !== 'undefined' ? window.location.href : '/'} />
         </div>
     );
 }
